059b7633fa278191788bdd4e60ad3c559441cb12
0x3lden <153280243+0x3lden@users.noreply.github.com>
2025-06-27 12:05:15 +0200

    Solves compilation error

--- src/services/tokenData/computers/implementations/ERC20/ERC20DataComputer.ts
+++ src/services/tokenData/computers/implementations/ERC20/ERC20DataComputer.ts
@@ -8,45 +8,45 @@ export abstract class ERC20DataComputer extends TokenDataComputer {
   private address: string;
   private ERC20_ABI = [
     {
-      "type": "function",
-      "name": "name",
-      "inputs": [],
-      "outputs": [{ "type": "string" }]
+      type: "function",
+      name: "name",
+      inputs: [],
+      outputs: [{ type: "string" }],
     },
     {
-      "type": "function",
-      "name": "symbol",
-      "inputs": [],
-      "outputs": [{ "type": "string" }]
+      type: "function",
+      name: "symbol",
+      inputs: [],
+      outputs: [{ type: "string" }],
     },
     {
-      "type": "function",
-      "name": "totalSupply",
-      "inputs": [],
-      "outputs": [{ "type": "uint256" }]
+      type: "function",
+      name: "totalSupply",
+      inputs: [],
+      outputs: [{ type: "uint256" }],
     },
     {
-      "type": "function",
-      "name": "decimals",
-      "inputs": [],
-      "outputs": [{ "type": "uint8" }]
+      type: "function",
+      name: "decimals",
+      inputs: [],
+      outputs: [{ type: "uint8" }],
     },
     {
-      "type": "function",
-      "name": "balanceOf",
-      "inputs": [{ "type": "address" }],
-      "outputs": [{ "type": "uint256" }]
-    }
+      type: "function",
+      name: "balanceOf",
+      inputs: [{ type: "address" }],
+      outputs: [{ type: "uint256" }],
+    },
   ];
   private underlyingTokenAddress: string;
 
-  constructor(private tokenDefinition: TokenDefinition) {
+  constructor(tokenDefinition: TokenDefinition) {
     super();
-    
+
     // Initialize properties after constructor
     this.address = tokenDefinition.L1WrappedTokenAddress;
     this.underlyingTokenAddress = tokenDefinition.L1UnderlyingTokenAddress;
-    
+
     this.provider = createPublicClient({
       chain: mainnet,
       transport: http(this.getRpcUrl()),
@@ -54,53 +54,53 @@ export abstract class ERC20DataComputer extends TokenDataComputer {
   }
 
   async name(): Promise<string> {
-    const name = await this.provider.readContract({
+    const name = (await this.provider.readContract({
       address: this.address as `0x${string}`,
       abi: this.ERC20_ABI,
       functionName: "name",
-    }) as string;
+    })) as string;
     console.log("name", name);
     return name;
   }
 
   async symbol(): Promise<string> {
-    const symbol = await this.provider.readContract({
+    const symbol = (await this.provider.readContract({
       address: this.address as `0x${string}`,
       abi: this.ERC20_ABI,
       functionName: "symbol",
-    }) as string;
+    })) as string;
     console.log("symbol", symbol);
     return symbol;
   }
 
   async totalSupply(): Promise<number> {
-    const totalSupply = await this.provider.readContract({
+    const totalSupply = (await this.provider.readContract({
       address: this.address as `0x${string}`,
       abi: this.ERC20_ABI,
       functionName: "totalSupply",
-    }) as bigint;
-    const decimals = await this.provider.readContract({
+    })) as bigint;
+    const decimals = (await this.provider.readContract({
       address: this.address as `0x${string}`,
       abi: this.ERC20_ABI,
       functionName: "decimals",
-    }) as bigint;
+    })) as bigint;
     console.log("totalSupply", totalSupply);
     console.log("decimals", decimals);
     return Number(totalSupply) / 10 ** Number(decimals);
   }
 
   async totalSupplyUSDC(): Promise<number> {
-    const totalSupplyUSDC = await this.provider.readContract({
+    const totalSupplyUSDC = (await this.provider.readContract({
       address: this.underlyingTokenAddress as `0x${string}`,
       abi: this.ERC20_ABI,
       functionName: "balanceOf",
       args: [this.address as `0x${string}`],
-    }) as bigint;
-    const decimalsUSDC = await this.provider.readContract({
+    })) as bigint;
+    const decimalsUSDC = (await this.provider.readContract({
       address: this.underlyingTokenAddress as `0x${string}`,
       abi: this.ERC20_ABI,
       functionName: "decimals",
-    }) as bigint;
+    })) as bigint;
     return Number(totalSupplyUSDC) / 10 ** Number(decimalsUSDC);
   }
 }
